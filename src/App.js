ac1a88fd9ae6abe5b3629ec47fecb9bf4c27b242
Keerthana <37702583+ParsaKeerthana@users.noreply.github.com>
2023-10-27 15:32:40 -0700

    frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
 import './App.css';
 
+const socket = io.connect('http://localhost:3001');
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [name, setName] = useState('');
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        socket.on('message', (data) => {
+            setMessages((prevMessages) => [...prevMessages, data]);
+        });
+
+        return () => {
+            socket.off('message');
+        }
+    }, []);
+
+    const sendMessage = (e) => {
+        e.preventDefault();
+        if (name && message) {
+            socket.emit('message', { name, message });
+            setMessage('');
+        }
+    };
+
+    return (
+        <div className="App">
+            <div className="messages">
+                {messages.map((msg, index) => (
+                    <div key={index} className={msg.name === name ? 'myMessage' : 'otherMessage'}>
+                        <strong>{msg.name}: </strong>{msg.message}
+                    </div>
+                ))}
+            </div>
+            <div className="inputSection">
+                <input value={name} onChange={(e) => setName(e.target.value)} placeholder="Name" />
+                <br />
+                <input value={message} onChange={(e) => setMessage(e.target.value)} placeholder="Message" />
+                <button onClick={sendMessage}>Send</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
